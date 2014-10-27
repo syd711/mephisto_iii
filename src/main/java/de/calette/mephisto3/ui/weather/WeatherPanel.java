60e6b412da9ae5ec0309205872cd338fe745ad24
syd711 <syd711@googlemail.com>
2014-10-27 20:26:52 +0100

    Finally finished weather icons

--- src/main/java/de/calette/mephisto3/ui/weather/WeatherPanel.java
+++ src/main/java/de/calette/mephisto3/ui/weather/WeatherPanel.java
@@ -6,33 +6,28 @@ import callete.api.services.weather.Weather;
 import de.calette.mephisto3.Mephisto3;
 import de.calette.mephisto3.ui.ControllablePanel;
 import de.calette.mephisto3.util.TransitionUtil;
+import javafx.animation.Transition;
 
 /**
  */
 public class WeatherPanel extends ControllablePanel {
+
+  private Transition transition;
+
   public WeatherPanel() {
     super(Callete.getWeatherService().getWeather());
     setMinWidth(Mephisto3.WIDTH);
+
+    for (ServiceModel model : models) {
+      WeatherLocationPanel panel = new WeatherLocationPanel((Weather) model);
+      getChildren().add(panel);
+    }
+
+    this.transition = TransitionUtil.createInFader(this);
   }
 
   @Override
   public void showPanel() {
-    TransitionUtil.createInFader(this).play();
-
-    for (ServiceModel model : models) {
-      WeatherLocationPanel panel = new WeatherLocationPanel((Weather) model);
-      getChildren().add(panel);
-      panel.show();
-    }
-  }
-
-  @Override
-  public void hidePanel() {
-    this.setOpacity(0);
-    for (ServiceModel model : models) {
-      WeatherLocationPanel panel = new WeatherLocationPanel((Weather) model);
-      getChildren().add(panel);
-      panel.hidePanel();
-    }
+    transition.play();
   }
 }
